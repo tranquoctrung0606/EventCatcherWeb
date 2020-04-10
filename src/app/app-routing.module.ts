ea5396c53a0a91e89cfd5014cfcc5afca82a59ff
=tranquoctrung0606 <=trungtran9866@gmail.com>
2020-04-10 11:53:41 +0700

    router fail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CreateEventComponent } from './create-event/create-event.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 const routes: Routes = [
   {path:'login', component: LoginComponent},
   {path:'create-event', component: CreateEventComponent},
+  {path:'sign-up', component:SignupComponent},
   { path: '', redirectTo: 'login', pathMatch: 'full' }
   
 ];
