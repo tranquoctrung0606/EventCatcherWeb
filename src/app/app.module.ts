06f43070d22ae2ff63047105e235b4ebfe2d9dde
=tranquoctrung0606 <=trungtran9866@gmail.com>
2020-04-11 11:48:54 +0700

    signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CreateEventComponent } from './create-event/create-event.component';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import firebaseConfig from './firebase'
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +20,10 @@ import { SignupComponent } from './signup/signup.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AngularFireAuth } from '@angular/fire/auth';
+import {auth} from 'firebase/app'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username: string=""
+  password: string=""
+  constructor(public afAuth: AngularFireAuth) { }
 
   ngOnInit(): void {
   }
-
+   async login(){
+    const {username, password} =this
+    try {
+      const  res= await this.afAuth.signInWithEmailAndPassword(username +'@gmail.com', password)
+      console.dir(res)
+    } catch (error) {
+      console.dir(error)
+     
+    }
+  }
 }
